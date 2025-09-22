3d9341e10f60c1a81c3562eb9f9ff1cbfdaf25c7
DafneNahir <dlm_28@hotmail.com>
2025-09-22 08:48:16 -0300

    Modificaciones en detalles de visualización

--- js/main.js
+++ js/main.js
@@ -31,37 +31,37 @@ function menu() {
 menu();
 
 const verStock = () => {
- return alert("Leche: " + stock[0] + "\n Pan: " + stock[1] + "\n Huevos: " + stock[2]);
+  return alert("Leche: " + stock[0] + "\n Pan: " + stock[1] + "\n Huevos: " + stock[2]);
 }
 
 function usarProducto() {
   let opcion = parseInt(prompt("¿Qué querés usar? \n 1 = Leche \n 2 = Pan \n 3 = Huevos \n 4 = Salir"));
-  if (opcion >=1 && opcion <= 3){
+  if (opcion >= 1 && opcion <= 3) {
     let cantidad = parseInt(prompt("¿Cuánto vas a usar de " + productos[opcion - 1] + "?"));
-      if (stock[opcion - 1] >= cantidad) {
-        stock[opcion - 1] -= cantidad;
-        alert("Usaste " + cantidad + " unidades " + productos[opcion - 1] + ".");
-      } else {
-        alert("No hay suficiente stock de " + productos[opcion - 1] + ".");
-      }
-  } else if (opcion === 4){
-      alert("No utilizaste nada esta vez. ¡Entonces vamos a reponer!");
+    if (stock[opcion - 1] >= cantidad) {
+      stock[opcion - 1] -= cantidad;
+      alert("Usaste " + cantidad + " unidades " + productos[opcion - 1] + ".");
+    } else {
+      alert("No hay suficiente stock de " + productos[opcion - 1] + ".");
+    }
+  } else if (opcion === 4) {
+    alert("No utilizaste nada esta vez. ¡Entonces vamos a reponer!");
   } else {
-    alert ("Elija un número correcto, por favor.")
+    alert("Elija un número correcto, por favor.")
   }
 }
 
 function reponerProducto() {
   let opcion = parseInt(prompt("¿Qué querés reponer? \n 1 = Leche \n 2 = Pan \n 3 = Huevos \n 4 = Salir"));
-  if (opcion === 4){
-    alert ("No repusiste nada esta vez. ¡Terminamos por hoy!")
+  if (opcion === 4) {
+    alert("No repusiste nada esta vez. ¡Terminamos por hoy!")
     return
   }
-  if (opcion >=1 && opcion <= 3) { 
+  if (opcion >= 1 && opcion <= 3) {
     let cantidad = parseInt(prompt("¿Cuánto vas a reponer de " + productos[opcion - 1] + "?"));
-    stock[opcion -1 ] += cantidad;
+    stock[opcion - 1] += cantidad;
     alert("Agregaste " + cantidad + " unidades de " + productos[opcion - 1] + " al stock.");
-  } 
+  }
 }
 
 alert("Tu stock final es:\n" + "Leche: " + stock[0] + "\nPan: " + stock[1] + "\nHuevos: " + stock[2]);    
